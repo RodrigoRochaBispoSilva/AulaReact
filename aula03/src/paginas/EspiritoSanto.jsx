62b4deea18e7ec4abfd6311574b573a780085ea1
RodrigoRochaBispoSilva <rodrigorochabispo@gmail.com>
2026-07-28 11:37:48 -0300

    28/7

---
+++ aula03/src/paginas/EspiritoSanto.jsx
@@ -0,0 +1,9 @@
+export default function EspiritoSanto()
+{
+    return (
+        <div>
+            <h1>Estado do Espirito Santo</h1>
+        </div>
+    );
+}
+
---
+++ aula03/src/paginas/SaoPaulo.jsx
@@ -0,0 +1,8 @@
+export default function SaoPaulo()
+{
+    return (
+        <div>
+            <h1>Estado de Sao Paulo</h1>
+        </div>
+    );
+}
